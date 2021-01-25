6488243b9a96f0208bb082bcc6d18658ae30f35e
Kukwa, Andrew (IMS) <kukwaa@imsweb.com>
2021-01-25 13:32:22 -0500

    Update version number.

--- chromepdf/__init__.py
+++ chromepdf/__init__.py
@@ -1,5 +1,5 @@
 # Make sure to update the string in setup.py also (version='...')
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 
 from chromepdf.exceptions import ChromePdfException
 from chromepdf.maker import ChromePdfMaker
