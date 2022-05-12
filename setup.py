6a37000a126f972ce90c0fec9a086118f3537078
Kukwa, Andrew (IMS) <kukwaa@imsweb.com>
2022-05-12 09:37:21 -0400

    Stop setuptools from installing the "testapp" folder.

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     author_email='kukwaa@imsweb.com',
     url='https://github.com/imsweb/django-chromepdf',
     license='BSD',
-    packages=find_packages(exclude=('testapp',)),
+    packages=find_packages(exclude=['testapp*']),
     include_package_data=True,
     classifiers=[
         'Development Status :: 5 - Production/Stable',
